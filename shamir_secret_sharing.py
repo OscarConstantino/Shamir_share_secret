11159698fe29daeaaf868ea54fbad8e0d902d691
Oscar Constantino <12437469+OscarConstantino@users.noreply.github.com>
2025-01-20 01:38:22 +0000

    Updating the methods and constructor actions

--- shamir_secret_sharing.py
+++ shamir_secret_sharing.py
@@ -2,7 +2,6 @@ from os import urandom
 from random import sample
 from math import sqrt
 
-#Shamir class with the necessary operations.
 class Shamir:
     secret = 0
     n_parts = 0
@@ -11,8 +10,12 @@ class Shamir:
     points = []
     prime_number = 2
 
-    def __init__(self,secret, n_parts, k_parts):
+    def __init__(self):
         #Validating that secret is an integer
+        Shamir.points = []
+        Shamir.polynomial = []
+
+    def get_values(self, secret, n_parts, k_parts):
         if secret.isdigit() and n_parts.isdigit() and k_parts.isdigit():
             Shamir.secret, Shamir.n_parts, k_parts = int(secret), int(n_parts), int(k_parts)
             Shamir.prime_number = Shamir.generate_prime_number(self,5)
@@ -53,7 +56,6 @@ class Shamir:
 
     def polynomial_construction(self):
         Shamir.polynomial = [Shamir.secret] + [ Shamir.random_number_coeff_selection(self, 5, 1) for _ in range(Shamir.k_parts - 1) ]
-        print(Shamir.polynomial)
     
     def random_number_coeff_selection(self, size, iteration):
         random_number = int.from_bytes(urandom(size))
@@ -77,21 +79,20 @@ class Shamir:
     def modular_inverse(self, a, p):
         return pow(a, -1, p)
 
-    def lagrange_basis_calculation(self, x_shares, j):
+    def lagrange_basis_calculation(self, x_shares, j, p_number):
         lagrange_base = 1
         x_j = x_shares[j]
         for m, x_m in enumerate(x_shares):
             if m != j:
-                numerator = (0 - x_m) % Shamir.prime_number
-                denominator = (x_j - x_m) % Shamir.prime_number
-                lagrange_base = (lagrange_base * numerator * Shamir.modular_inverse(self, denominator, Shamir.prime_number)) % Shamir.prime_number
+                numerator = (0 - x_m) % p_number
+                denominator = (x_j - x_m) % p_number
+                lagrange_base = (lagrange_base * numerator * Shamir.modular_inverse(self, denominator, p_number)) % p_number
         return lagrange_base
 
-    def reconstruct_secret_shamir(self, min_points):
-        print("POINTS USED: ", min_points)
+    def reconstruct_secret_shamir(self, min_points, p_number):
         x_shares, y_shares = zip(*min_points)
         secret = 0
         for j in range(len(min_points)):
-            l_b = Shamir.lagrange_basis_calculation(self, x_shares, j)
-            secret = (secret + y_shares[j] * l_b) % Shamir.prime_number
+            l_b = Shamir.lagrange_basis_calculation(self, x_shares, j, p_number)
+            secret = (secret + y_shares[j] * l_b) % p_number
         return secret
